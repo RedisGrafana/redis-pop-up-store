51b53a9146e7a3249dbfd19fd307fd01159774a3
Mikhail <47795110+mikhailredis@users.noreply.github.com>
2020-07-12 17:36:44 -0700

    Update gears-execute.sh

--- gears-execute.sh
+++ gears-execute.sh
@@ -1,5 +1,5 @@
-# Install Trigger to add Queue to Time-Series
+# Install StreamReader to add Queue to Time-Series
 cat gears/timeseries.py | docker exec -i redismod redis-cli -x RG.PYEXECUTE
 
-# Install Trigger to complete orders
+# Install StreamReader to complete orders
 cat gears/complete.py | docker exec -i redismod redis-cli -x RG.PYEXECUTE
