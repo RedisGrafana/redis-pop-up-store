d1ebc45aa4ad92f286fd5e10b7cd5cdd5bfa9a58
Mikhail <47795110+mikhailredis@users.noreply.github.com>
2021-10-11 15:31:10 -0400

    Update Readme, docker and packages

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Change Log
+
+## 2.0.0 (2021-10-11)
+
+### Features / Enhancements
+
+- Added Streaming dashboard
+- Update screenshots and dashboards for Grafana 8.2
+- Added demo on https://demo.volkovlabs.io
+
+## v1.0.0 (2020-07-12)
+
+### Features / Enhancements
+
+- Initial release
--- gears-execute.sh
+++ gears-execute.sh
@@ -1,5 +1,5 @@
 # Install StreamReader to add Queue to Time-Series
-cat gears/timeseries.py | docker exec -i redismod redis-cli -x RG.PYEXECUTE
+cat gears/timeseries.py | docker exec -i redis redis-cli -x RG.PYEXECUTE
 
 # Install StreamReader to complete orders
-cat gears/complete.py | docker exec -i redismod redis-cli -x RG.PYEXECUTE
+cat gears/complete.py | docker exec -i redis redis-cli -x RG.PYEXECUTE
