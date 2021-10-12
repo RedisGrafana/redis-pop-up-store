75dea9988a133c870e0e3cc82b97771d9ef92596
Mikhail <47795110+mikhailredis@users.noreply.github.com>
2021-10-12 11:14:37 -0400

    Update script and add demo

--- gears/orders.py
+++ gears/orders.py
@@ -5,8 +5,12 @@ def complete(x):
     execute('XDEL', 'queue:customers', x['value']['customer'])
     execute('DECR', 'product')
 
+    product = execute('GET', 'product')
+    if int(product) < 1000:
+        execute('SET', 'product', 10000)
+
 
 # Stream Reader for Orders queue
 gb = GearsBuilder('StreamReader')
 gb.map(complete)
-gb.register(prefix='queue:orders', batch=3, trimStream=True)
+gb.register(prefix='orders', batch=3, trimStream=True)
